a83bb288ba96a90fb375f301d909f5141e28f7f8
Eric Shankman <ershankman@gmail.com>
2025-04-24 09:53:50 -0400

    fixed styling

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,24 +3,23 @@ import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 export default function NavBar() {
+  //prettier-ignore
   return (
-    //prettier-ignore
     <nav className={styles.nav}>
-      <div className={styles.navLeft}>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/vscode-extensions">VS Code Extensions</Link></li>
-          <li><Link to="/vscode-shortcuts">VS Code Shortcuts</Link></li>
-          <li><Link to="/github">Git/GitHub</Link></li>
-          {/* <li><Link to="/youtube">YouTube Playlists</Link></li> */}
-          <li><Link to="/documentation">Documentation</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li><Link to="/login">Login</Link></li>
-        </ul>
-      </div>
-      <div className={styles.navRight}>
-        {/* Any right-side content here */}
-      </div>
-    </nav>
-  );
+  <div className={styles.navLeft}>
+    <span className={styles.logo}>FTRI</span>
+  </div>
+  <div className={styles.navRight}>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/vscode-extensions">VS Code Extensions</Link></li>
+      <li><Link to="/vscode-shortcuts">VS Code Shortcuts</Link></li>
+      <li><Link to="/github">Git/GitHub</Link></li>
+      <li><Link to="/documentation">Documentation</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+      <li><Link to="/login">Login</Link></li>
+    </ul>
+  </div>
+</nav>
+  )
 }
